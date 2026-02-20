1e01a66c7b3fac0921b738cf4b97e67c1f8b94a0
Aasta-EB <286852@stud.inn.no>
2026-02-20 09:10:58 +0100

    adding what code Dag wanted for the comp

--- MathComp2.2026/Classes/Matrix.cpp
+++ MathComp2.2026/Classes/Matrix.cpp
@@ -1,1 +1,38 @@
 #include "Matrix.h"
+
+void Matrix::matrix()
+{
+}
+
+void Matrix::read()
+{
+}
+
+void Matrix::print()
+{
+}
+
+Matrix Matrix::multiply(Matrix other)
+{
+	return Matrix();
+}
+
+Matrix Matrix::transpose()
+{
+	return Matrix();
+}
+
+double Matrix::determinant()
+{
+	return 0.0;
+}
+
+Matrix Matrix::inverse()
+{
+	return Matrix();
+}
+
+Vector3D Matrix::multiply(Vector3D v)
+{
+	return Vector3D();
+}
--- MathComp2.2026/Classes/Matrix.h
+++ MathComp2.2026/Classes/Matrix.h
@@ -1,5 +1,24 @@
 #pragma once
+#include "Vector3D.h"
+
 class Matrix
 {
+
+public: 
+    void matrix();
+    void read();
+    void print();
+    Matrix multiply(Matrix other);
+    Matrix transpose();
+    double determinant();
+    Matrix inverse();
+    Vector3D multiply(Vector3D v);
+private:
+    static const int M = 10;
+    static const int N = 10;
+    double A[M][N];
+    int m; // actual number of rows
+    int n; // actual number of columns
+
 };
 
